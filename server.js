7f86012d124ead62e23500b3e9ac5fa58e02311a
wahidman <wahidman948@gmail.com>
2025-02-13 13:59:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ app.use(bodyParser.json());
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Untuk menghindari error SSL
+        }
+    }
 });
 
 // Definisi Model Order
